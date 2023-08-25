9f1178c0ca31e13db46d0ca1682626e0610545c4
najuliag <ajgalmeida12345@gmail.com>
2023-08-25 17:39:24 -0300

    tratando erros e fazendo uma calculadora

--- src/main/java/com/example/demo/MathController.java
+++ src/main/java/com/example/demo/MathController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.Exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,71 @@ public class MathController {
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "numberOne") String numberOne,
-                      @PathVariable(value = "numberTwo") String numberTwo){
-        return 1D;
+                      @PathVariable(value = "numberTwo") String numberTwo)
+    throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, set a numeric value.");
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+
+    @RequestMapping(value = "/sub/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sub(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, set a numeric value.");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+
+    }
+    @RequestMapping(value = "/mult/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double mult(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo)
+            throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, set a numeric value.");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/div/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double div(@PathVariable(value = "numberOne") String numberOne,
+                       @PathVariable(value = "numberTwo") String numberTwo)
+            throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, set a numeric value.");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/med/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double med(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo)
+            throws Exception{
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new UnsupportedMathOperationException("Please, set a numeric value.");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) / 2;
+    }
+
+    @RequestMapping(value = "/squareroot/{numberOne}", method = RequestMethod.GET)
+    public Double squareRoot(@PathVariable(value = "numberOne")String numberOne) throws Exception{
+        if(!isNumeric(numberOne)){
+            throw new UnsupportedMathOperationException("Please, set a numeric value.");
+        }
+        return Math.sqrt(convertToDouble(numberOne));
+    }
+
+    public Double convertToDouble(String strNumber){
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+    public boolean isNumeric(String strNumber){
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
